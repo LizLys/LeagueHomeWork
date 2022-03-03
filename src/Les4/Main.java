dcccd9a84424625dec1d3364af29688d1453b073
LizLys <lizalysenko@yandex.ru>
2022-03-03 13:37:08 +0300

    Внесены правки

---
+++ src/Les4/Main.java
@@ -0,0 +1,58 @@
+package Les4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите одно из действий: Add - добавить товар на склад, " +
+                "Delete - удалить товар со склада, Concurrence" +
+                " - проверка наличия товара на складе, Number - количество товаров на складе, Uchet - количество " +
+                "экземпляров данного товара, End - выход из программы.");
+
+        Sclad sclad = new Sclad();
+        boolean a = false;
+
+        while (!a) {
+            String userAction = console.nextLine();
+            switch (userAction) {
+                case "Add":
+                    System.out.println("Пожалуйста, введите название товара, " +
+                            "который вы хотите добавить на склад:");
+                    String object = console.nextLine();
+                    sclad.add(object);
+                    break;
+                case "Delete":
+                    System.out.println("Пожалуйста, введите название товара, " +
+                            "который вы хотите удалить со склада:");
+                    String object1 = console.nextLine();
+                    sclad.delete(object1);
+
+                    break;
+                case "Concurrence":
+                    System.out.println("Пожалуйста, введите название товара, " +
+                            "наличие которого вы хотите проверить:");
+                    String object2 = console.nextLine();
+                    sclad.concurrence(object2);
+                    break;
+                case "Number":
+                    sclad.number();
+                    break;
+                case "Uchet":
+                    System.out.println("Пожалуйста, введите название товара, " +
+                            "количество которого вы хотите посмотреть на складе:");
+                    String object4 = console.nextLine();
+                    sclad.uchet(object4);
+                    break;
+                default:
+                    if (userAction.equals("End")) {
+                        a = true;
+                    } else {
+                        System.out.println("Программа продолжает работать. Введите команду.");
+                    }
+            }
+        }
+    }
+}
+
--- src/Les4/Sclad.java
+++ src/Les4/Sclad.java
@@ -7,66 +7,45 @@ import java.util.*;
 
 public class Sclad {
 
-    Scanner console = new Scanner(System.in);
-    String object = console.nextLine();
     List<String> sklad = new ArrayList<>(30);
 
 
     //Добавление объектов
-    protected void add(String object) {
-        String add = "Add";
-        this.object = object;
-        if (object.equals(add)) {
-            System.out.println("Пожалуйста, введите название товара, " +
-                    "который вы хотите добавить на склад:");
-            Scanner console1 = new Scanner(System.in);
-            String newObject = console1.nextLine();
-            sklad.add(newObject);
-            //доп задание на проверку свободного места на складе.
-            //Наш список ограничен 30-ю значениями, поэтому танцуем от этого
-            if (sklad.lastIndexOf(newObject)==30) {
-                System.out.println("Склад полностью заполнен. При добавлении новых товаров, склад расширится автоматически." +
-                        "Если вы хотите добавить еще товары, введите Add, для удаления введите Delete.");
-            }
+    void add(String object) {
+        sklad.add(object);
+        System.out.println("Товар " + object + " успешно добавлен на склад под индексом " + sklad.lastIndexOf(object));
+        //доп задание на проверку свободного места на складе.
+        //Наш список ограничен 30-ю значениями, поэтому танцуем от этого
+        if (sklad.lastIndexOf(object) == 30) {
+            System.out.println("Склад полностью заполнен. При добавлении новых товаров, склад расширится автоматически." +
+                    "Если вы хотите добавить еще товары, введите Add, для удаления введите Delete.");
         }
     }
 
     //удаление товаров со склада
-    protected void delete(String object) {
-        String delete = "Delete";
-        this.object = object;
-        if (object.equals(delete)) {
-            System.out.println("Пожалуйста, введите название товара, " +
-                    "который вы хотите удалить со склада:");
-            Scanner console1 = new Scanner(System.in);
-            String newObject = console1.nextLine();
-            sklad.remove(newObject);
+    void delete(String object1) {
+        if (sklad.indexOf(object1)>-1){
+            sklad.remove(object1);
+            System.out.println("Товар " + object1 + " успешно удален.");
+        } else {
+            System.out.println("Данного товара на складе не обнаружено.");
+        }
+        }
+
+
+    //Проверка наличия товара на складе
+    void concurrence(String object2) {
+        if (sklad.indexOf(object2) == -1) {
+            System.out.println("Данный товар на складе не обнаружен.");
+        } else {
+            System.out.println("Ваш товар хранится на складе под индексом " + sklad.indexOf(object2));
         }
     }
 
-    //Проверка наличия товара на складе
-    protected void concurrence(String object) {
-        String concurrence = "Concurrence";
-        this.object = object;
-        if (object.equals(concurrence)) {
-            System.out.println("Пожалуйста, введите название товара, " +
-                    "наличие которого вы хотите проверить:");
-            Scanner console1 = new Scanner(System.in);
-            String newObject = console1.nextLine();
-            if (sklad.indexOf(newObject) == -1) {
-                System.out.println("Данный товар на складе не обнаружен.");
-            } else {
-                System.out.println("Ваш товар хранится на складе под индексом " + sklad.indexOf(newObject));
-            }
-        }
-    }
 
     //Проверка количества товаров на складе. 1 товар - 1 индекс =>
     // количество объектов определяется последним индексом хранимого объекта
-    protected void number(int number) {
-        String numberForSearch = "Number";
-        this.object = object;
-        if (object.equals(numberForSearch)) {
+    protected void number() {
             System.out.print("Количество товара на складе - ");
             if (sklad.indexOf(null) == 0) {
                 System.out.println(0);
@@ -74,38 +53,28 @@ public class Sclad {
                 int a = sklad.indexOf(null) + 1;
                 System.out.print(a);
             }
-        }
     }
 
     //реализация учета количества каждого элемента на складе
-    protected void uchet(String object) {
-        String uchet = "Uchet";
-        this.object = object;
-        if (object.equals(uchet)) {
-            System.out.println("Пожалуйста, введите название товара, " +
-                    "количество которого вы хотите посмотреть на складе:");
-            Scanner console1 = new Scanner(System.in);
-            String newObject = console1.nextLine();
-            sklad.indexOf(newObject);
-            sklad.lastIndexOf(newObject);
-            int x = 0;
-            if (sklad.indexOf(newObject) == -1) {
-                System.out.println("Данный товар отсутствует.");
-            } else if (sklad.indexOf(newObject) == sklad.lastIndexOf(newObject)) {
-                System.out.println("Данный товар находится в единственном экземпляре");
-            } else {
-                System.out.println("у нас точно есть два экзмпляра данного товара, но сейчас поищем еще:");
-            for (int i=sklad.indexOf(newObject); i<sklad.lastIndexOf(newObject); i++)
-            {
-                if (object.equals(sklad.get(i+1))) {
-                    x=x+1;
-                    int y = x+2;
+    protected void uchet(String object4) {
+
+        sklad.indexOf(object4);
+        sklad.lastIndexOf(object4);
+        int x = 0;
+        if (sklad.indexOf(object4) == -1) {
+            System.out.println("Данный товар отсутствует.");
+        } else if (sklad.indexOf(object4) == sklad.lastIndexOf(object4)) {
+            System.out.println("Данный товар находится в единственном экземпляре");
+        } else {
+            System.out.println("у нас точно есть два экзмпляра данного товара, но сейчас поищем еще:");
+            for (int i = sklad.indexOf(object4); i < sklad.lastIndexOf(object4); i++) {
+                if (object4.equals(sklad.get(i + 1))) {
+                    x = x + 1;
+                    int y = x + 2;
                     System.out.println("Секундочку! Мы нашли данный товар в " + y + "экземплярах. Сейчас посмотрим еще!");
                 }
             }
-                System.out.println("Вроде, это все.");
-            }
+            System.out.println("Вроде, это все.");
         }
     }
-
 }
